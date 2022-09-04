7a7648154c90143bd676788786c410cb4a52f777
AnnaMintel <annikamintel99@yandex.ru>
2022-09-04 15:29:31 +0300

    refactor:  useMemo

--- src/components/Accordion/useMemo/useMemo.tsx
+++ src/components/Accordion/useMemo/useMemo.tsx
@@ -1,43 +1,30 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
-const NewMessagerCounter = (props: { count: number }) => {
-    return <div>{props.count}</div>
+export default {
+    title: "useMemo"
 }
 
-// const UsersSecret = (props: { users: Array<string> }) => {
-//     return <div>{
-//         props.users.map((u, i) => <div key={i}>{u}</div>)
-//     }
-//     </div>
-// }
-
-// const Users = React.memo(UsersSecret);
-
-//2nd way
-
-const Users =  React.memo ((props: { users: Array<string> }) => {
-    return <div>{
-        props.users.map((u, i) => <div key={i}>{u}</div>)
-    }
-    </div>
-})
-
-
-export const Example1 = () => {
-    const [counter, setCounter] = useState(0);
-    const [users, setUsers] = useState(["Helena" , "Krista", "Iviem"]);
-
-    const addUser = () => {
-        const newUsers = [...users, "Sveta" + new Date().getTime()];
-        setUsers(newUsers);
-    }
+export const DifficultCountingExanple = () => {
 
     return <>
-    <button onClick={ () => setCounter(counter + 1)}>+</button>
-    <button onClick={addUser}>add user</button>
 
-            <NewMessagerCounter count={counter} />
-            <Users users={users} />
     </>
+}
+
+export const HelpsForREactMemo = () => {
+
+    const [a, setA] = useState<number>(5)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            tempResultA = tempResultA * i
+        }
+        return tempResultA;
+    }, [a]);
 }
 
